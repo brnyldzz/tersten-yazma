19737354fc1b9af6e2763ee607512c895141ae94
Panaria <baranyldzby@gmail.com>
2023-10-11 16:35:52 +0300

    Add project files.

---
+++ tersten yazma/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tersten_yazma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcının girdiği bir kelimeyi tersten yazan programı yazınız.
+
+            string kelime;
+
+            Console.WriteLine("Kelime Giriniz");
+            kelime = Console.ReadLine();
+
+            for (int i = kelime.Length-1; i > -1; i--)
+            {
+                Console.Write(kelime[i]);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
